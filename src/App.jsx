d9ff5a5676662c91a34b4f659574223f453c1fd7
vahe-m <vmanukyan71@gmail.com>
2024-02-23 18:43:25 +0400

    Creating a music player with reactJS

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css';
+import { Layout, } from 'antd';
+const { Header, Footer, Content } = Layout;
+
+const layoutStyle = {
+  borderRadius: 8,
+  overflow: 'hidden',
+  width: 'calc(50% - 8px)',
+  maxWidth: 'calc(50% - 8px)',
+  margin: '0 auto',
+};
+const headerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: '#4096ff',
+};
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#0958d9',
+};
+const footerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#4096ff',
+};
+
+function App() {
+  return (
+    <Layout style={layoutStyle}>
+      <Header style={headerStyle}>Header</Header>
+      <Content style={contentStyle}>Content</Content>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+  );
+}
+
+export default App;
